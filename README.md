db70a3a6e736ab673d18d77401258e80060f3a49
Rajashree Choudhary <rajashreechoudhary@Rajashrees-MacBook-Air.local>
2024-04-16 17:53:45 -0600

    Updated without data

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# capstone
+# Problem Statement:
+## Overview
+This Project aims to map and analyze areas with limited access to affordable and nutritious food, known as food deserts. Within this project, we focus on understanding the impact of food deserts on local communities. By leveraging geospatial data analysis and machine learning algorithms, this initiative aims to uncover insights into the distribution and characteristics of food deserts. These findings can assist community leaders in developing strategies and interventions to address food insecurity and improve access to nutritious food options.
+
+## Inspiration:
+Inspiration for this project comes from a publication [Using Social Media to Predict Food Deserts in the United States Infodemiology Study of Tweets](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9297137/)
+
+## Objectives
+#### Food Desert Mapping Application:
+- Develop an interactive application to visually represent the geographic distribution of food deserts.
+#### Demographic Disparity Analysis:
+- Identify and analyze demographic groups disproportionately affected by food deserts.
+#### Impact Assessment:
+- Evaluate the socio-economic and health-related effects of food deserts on local communities.
+#### Model Development:
+- Utilize a Logistic Regression model to classify areas as food deserts or non-food deserts.
+- Employ Clustering models (e.g., K-means) to discover spatial patterns within food deserts.
+- Conduct Geospatial Data Analysis to analyze spatial data and visualize trends.
+#### Future Work: 
+#### Time-Series Analysis:
+- Explore time-series analysis techniques to showcase temporal trends over time, focusing on state-level data.
+
+### Sample Dataset Dictionary
+|Field|LongName|Description|
+|-----|--------|-----------|
+|CensusTract|Census tract|Census tract number|
+|State|State|State name|
+|County|County|County name|
+|Urban|Urban tract|Flag for urban tract|
+|POP2010|Population, tract total|Population count from 2010 census|
+|OHU2010|Housing units, total|Occupied housing unit count from 2010 census|
+|GroupQuartersFlag|Group quarters, tract with high share|Flag for tract where >=67%|
+|NUMGQTRS|Group quarters, tract population residing in, number|Count of tract population residing in group quarters|
+|PCTGQTRS|Group quarters, tract population residing in, share|Percent of tract population residing in group quarters|
+|LILATracts_1And10|Low income and low access tract measured at 1 mile for urban areas and 10 miles for rural areas|Flag for low-income and low access when considering low accessibilty at 1 and 10 miles|
+|LILATracts_halfAnd10|Low income and low access tract measured at 1/2 mile for urban areas and 10 miles for rural areas|Flag for low-income and low access when considering low accessibilty at 1/2 and 10 miles|
+|LILATracts_1And20|Low income and low access tract measured at 1 mile for urban areas and 20 miles for rural areas|Flag for low-income and low access when considering low accessibilty at 1 and 20 miles|
+|LILATracts_Vehicle|Low income and low access tract using vehicle access or low income and low access tract measured at 20 miles|Flag for low-income and low access when considering vehicle access or at 20 miles|
+|HUNVFlag|Vehicle access, tract with low vehicle access|Flag for tract where >= 100 of households do not have a vehicle, and beyond 1/2 mile from supermarket|
+|LowIncomeTracts|Low income tract|Flag for low income tract|
+|PovertyRate|Tract poverty rate|Share of the tract population living with income at or below the Federal poverty thresholds for family size|
+|MedianFamilyIncome|Tract median family income|Tract median family income|
+|LA1and10|Low access tract at 1 mile for urban areas and 10 miles for rural areas|Flag for low access tract at 1 mile for urban areas or 10 miles for rural areas|
+|LAhalfand10|Low access tract at 1/2 mile for urban areas and 10 miles for rural areas|Flag for low access tract at 1/2 mile for urban areas or 10 miles for rural areas|
+- Read more about [Data dictionary](../csv_data/food_data_dictionary.csv)
+#### <u>Data Sources</u>
+- [Food Desert Data](https://www.ers.usda.gov/data-products/food-access-research-atlas/download-the-data/)
+- [500_Cities_data](https://data.cdc.gov/widgets/sfcy-rqbb)
+
+Shape of the data: 72531, 147
+#### The following Python libraries has been used for this project:
+
+- pandas
+- numpy
+- matplotlib
+- seaborn
+- geopandas
+- scikit-learn
+### [Interactive Map:](https://datawrapper.dwcdn.net/qiQhy/3/)
+<div style="min-height:500px"><script type="text/javascript" defer src="https://datawrapper.dwcdn.net/qiQhy/embed.js?v=3" charset="utf-8"></script><noscript><img src="https://datawrapper.dwcdn.net/qiQhy/full.png" alt="% of Food Insecurity" /></noscript></div>
+
+
+#### Reference
+- Lasso Feature [Code Reference](https://towardsdatascience.com/feature-selection-techniques-in-machine-learning-with-python-f24e7da3f36e)
---
+++ src/modeling_functions.py
@@ -0,0 +1,188 @@
+# import necessary packages
+import pandas as pd
+import numpy as np
+import os
+
+# visualization packages
+import matplotlib.pyplot as plt
+
+# modeling
+import scipy.stats as stats
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import RFECV
+from sklearn import metrics
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+from xgboost import XGBRegressor
+import xgboost as xgb
+from xgboost import XGBClassifier
+from xgboost import plot_importance
+
+# import function from other module to save figures
+from .functions import save_fig
+
+# function that prints neat formatted scoring metrics for model scoring
+def get_model_scores(model, y_train, y_train_pred, y_test, y_test_pred, ):
+    '''Enter model type, y_train, y_train_pred, y_test, y_test_pred.
+    Returns modeling scores printed out in a consistent format'''
+    print('\033[1m' + 'Below are the scoring metrics for {} model'.format(model) + '\033[0m')
+    print('\n')
+    print('Train {} Accuracy: {}'.format(model, metrics.accuracy_score(y_train, y_train_pred)))
+    print('Test {} Accuracy: {}'.format(model, metrics.accuracy_score(y_test, y_test_pred)))
+    print('\n')
+    print('Train {} F1: {}'.format(model, metrics.f1_score(y_train,y_train_pred)))
+    print('Test {} F1: {}'.format(model, metrics.f1_score(y_test,y_test_pred)))
+    print('\n')
+    print('Train {} Recall: {}'.format(model, metrics.recall_score(y_train, y_train_pred)))
+    print('Test {} Recall: {}'.format(model, metrics.recall_score(y_test, y_test_pred)))
+    print('\n')
+    print('Train {} Precision: {}'.format(model, metrics.precision_score(y_train, y_train_pred)))
+    print('Test {} Precision: {}'.format(model, metrics.precision_score(y_test, y_test_pred)))
+
+# function for plotting confusion matrix
+def confusion_matrix(estimator, X, y, title, display_labels=['Not Food Desert', 'Food Desert'],\
+    normalize='all'):
+    '''Function that plots confusion matrix for model
+    Enter estimator, X, y
+    Display labels and normalize have defaults but can be altered'''
+    fig, ax = fig, ax = plt.subplots(figsize=(12, 10))
+    plt.rcParams.update({'font.size': 16})
+    metrics.plot_confusion_matrix(estimator, X, y, 
+                              cmap=plt.cm.Blues, 
+                              display_labels=display_labels, 
+                              ax=ax,
+                              normalize=normalize)
+    ax.set_ylabel('True', fontdict={'fontsize':18})
+    ax.set_xlabel('Predicted', fontdict={'fontsize':18})
+    ax.tick_params(axis='both', which='major', labelsize=16)
+    plt.title(title, fontdict={'fontsize':20})
+
+# function that geospatially visualizes predictions
+def create_choropleth_map_pred(df1, df2, df3, col1, col2, col3, cmap, title, ptcolor):
+    '''Creates a choropleth map with three dataframes for plotting predictions
+    Select df1 and col1 for negative class
+    Select df2 and col2 for positive class
+    Select df3 and col3 for predicted positive class
+    Enter cmap for desired color palette
+    Enter title for map title and image saved name'''
+
+    # create map figure and axes
+    fig, ax = plt.subplots(figsize=(50,40))
+
+    # call .plot() method on df
+    ax = df1.plot(column = col1, 
+                            cmap=cmap, 
+                            ax=ax,
+                            edgecolor='face',
+                            zorder=1,
+                            alpha=0.5,
+                            missing_kwds={
+                                "color": "lightgrey",
+                            })
+
+    df2.plot(column = col2,
+                            color=ptcolor,
+                            ax=ax,
+                            zorder=3,
+                            alpha=0.65)
+    df3.plot(column = col3,
+                    color='yellow',
+                    ax=ax,
+                    zorder=2)
+    
+    # add annotation
+    ax.annotate("•", xy=(0.78, .895), size=90, xycoords='figure fraction', color='olive')
+    ax.annotate("True Positive", xy=(0.80, .90), size=70, xycoords='figure fraction')
+    
+    ax.annotate("•", xy=(0.78, .855), size=90, xycoords='figure fraction', color='yellow')
+    ax.annotate("False Positive", xy=(0.80, .86), size=70, xycoords='figure fraction')
+    
+    ax.annotate("•", xy=(0.78, .815), size=90, xycoords='figure fraction', color=ptcolor, alpha=0.65)
+    ax.annotate("Food Desert", xy=(0.80, .82), size=70, xycoords='figure fraction')
+    
+    ax.annotate("•", xy=(0.78, .765), size=90, xycoords='figure fraction', color='purple', alpha=0.5)
+    ax.annotate("Not Food Desert", xy=(0.80, .77), size=70, xycoords='figure fraction')
+    
+    ax.annotate("•", xy=(0.78, .725), size=90, xycoords='figure fraction', color='lightgrey')
+    ax.annotate("Missing Values", xy=(0.80, .72), size=70, xycoords='figure fraction')
+    
+    # set map title
+    ax.set_title(label=title,fontdict={'fontsize': 100}, loc='center')
+
+
+    # remove axis surrounding map
+    ax.set_axis_off()
+
+    # save map
+    save_fig(title.lower().replace(' ', '_'), transparent=True)
+    plt.show()
+
+# function that visualizes predictions and overlays FRESH geometry
+def create_choropleth_map_pred_fresh(df1, df2, df3, df4, col1, col2, col3, col4, cmap, title, ptcolor):
+    '''Creates a choropleth map with four dataframes for plotting predictions
+    Select df1 and col1 for negative class
+    Select df2 and col2 for positive class
+    Select df3 and col3 for predicted positive class
+    Select df4 and col4 for FRESH geometry to overlay
+    Enter cmap for desired color palette
+    Enter title for map title and image saved name'''
+
+    # create map figure and axes
+    fig, ax = plt.subplots(figsize=(50,40))
+
+    # call .plot() method on df
+    ax = df1.plot(column = col1, 
+                            cmap=cmap, 
+                            ax=ax,
+                            edgecolor='face',
+                            zorder=1,
+                            alpha=0.5,
+                            missing_kwds={
+                                "color": "lightgrey",
+                            })
+
+    df2.plot(column = col2,
+                            color=ptcolor,
+                            ax=ax,
+                            zorder=3,
+                            alpha=0.65)
+    df3.plot(column = col3,
+                    color='yellow',
+                    ax=ax,
+                    zorder=2)
+    
+    df4.plot(column = col4,
+            color='green',
+            ax=ax,
+            zorder=4,
+            alpha=.3)
+    
+    # add annotation
+    ax.annotate("•", xy=(0.78, .895), size=90, xycoords='figure fraction', color='olive')
+    ax.annotate("True Positive", xy=(0.80, .90), size=70, xycoords='figure fraction')
+    
+    ax.annotate("•", xy=(0.78, .855), size=90, xycoords='figure fraction', color='green', alpha=0.3)
+    ax.annotate("FRESH", xy=(0.80, .86), size=70, xycoords='figure fraction')
+    
+    ax.annotate("•", xy=(0.78, .815), size=90, xycoords='figure fraction', color=ptcolor, alpha=0.65)
+    ax.annotate("Food Desert", xy=(0.80, .82), size=70, xycoords='figure fraction')
+    
+    ax.annotate("•", xy=(0.78, .765), size=90, xycoords='figure fraction', color='purple', alpha=0.5)
+    ax.annotate("Not Food Desert", xy=(0.80, .77), size=70, xycoords='figure fraction')
+    
+    ax.annotate("•", xy=(0.78, .725), size=90, xycoords='figure fraction', color='lightgrey')
+    ax.annotate("Missing Values", xy=(0.80, .72), size=70, xycoords='figure fraction')
+    
+    # set map title
+    ax.set_title(label=title,fontdict={'fontsize': 100}, loc='center')
+
+
+    # remove axis surrounding map
+    ax.set_axis_off()
+
+    # save map
+    save_fig(title.lower().replace(' ', '_'), transparent=True)
+    plt.show()
